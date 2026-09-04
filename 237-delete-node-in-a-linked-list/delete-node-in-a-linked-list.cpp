21359c7e42bd0ccd34e0f611fbd76be0f8d36e1f
Gaurav30122004 <gauravmahto3012@gmail.com>
2026-09-05 01:21:17 +0530

    Time: 10 ms (34.94%) | Memory: 12.4 MB (22.54%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
@@ -9,22 +9,34 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        ListNode* prev = node;
-        ListNode* front = prev->next;
-        ListNode* beforeprev = nullptr;
 
-        while(front != NULL)
-        {
-            int temp= prev->val;
-            prev->val = front->val;
-            front->val = temp;
+        // tried to swap node val to its front and then when node val comes at last del that node 
+        // ListNode* prev = node;
+        // ListNode* front = prev->next;
+        // ListNode* beforeprev = nullptr;
 
-            beforeprev = prev;
-            prev= front;
-            front= front->next;
-        }
-        beforeprev->next = nullptr;
-        delete prev;
+        // while(front != NULL)
+        // {
+        //     int temp= prev->val;
+        //     prev->val = front->val;
+        //     front->val = temp;
+
+        //     beforeprev = prev;
+        //     prev= front;
+        //     front= front->next;
+        // }
+        // beforeprev->next = nullptr;
+        // delete prev;
         
+        ListNode* prev = nullptr;
+        while(node->next != nullptr)
+        {
+        node->val = node->next->val;
+        prev = node;
+        node= node->next;
+        }
+        prev->next = nullptr;
+        delete node;
+
     }
 };
