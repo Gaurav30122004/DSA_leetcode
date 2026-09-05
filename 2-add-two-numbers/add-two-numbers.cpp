f7465d9f3cd6e38c96e1fc5ac68209c60a51c377
Gaurav30122004 <gauravmahto3012@gmail.com>
2026-09-05 22:29:40 +0530

    Time: 4 ms (21.15%) | Memory: 77.2 MB (45.85%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -15,72 +15,32 @@ public:
 
         ListNode* t1 = l1;
         ListNode* t2 = l2;
-        int cnt1=0 , cnt2=0;
-        while(t1 != NULL || t2 != NULL)
-        {
-            if(t1 != NULL)
-            {
-                cnt1++;
-                t1= t1->next;
-            }
-            if(t2 != NULL){
-                 cnt2++;
-                 t2= t2->next;
-            }
-        }
 
-        ListNode* newHead = new ListNode();
-        ListNode* result = newHead;
-        int len = (cnt1>cnt2)? cnt1 : cnt2;
-        for(int i=0; i<len-1; i++)
-        {
-            ListNode* newNode = new ListNode();
-            result->next = newNode;
-            result = result->next;
-        }
-        result= newHead;
-
-        if(cnt1>= cnt2)
-        {
-            t1 = l1;
-            t2 = l2;
-        }
-        else{
-            t1= l2;
-            t2= l1;
-        }
+        ListNode* dummyNode = new ListNode(-1);
+        ListNode* curr = dummyNode;
         int carry=0;
-        bool carryFlag;
+
         while(t1 != NULL || t2 != NULL)
         {
-            int v1= (t1 != NULL)? t1->val:0;
-            int v2= (t2 != NULL)? t2->val:0;
-            int x = v1+v2 +carry;
-            carryFlag=0;
-            if(x<10){
-                result->val = x;
-                carryFlag =0;
-                carry=0;
-            }
-            else{
-                result->val = x%10;
-                carry = x/10;
-                carryFlag = 1;
-                
-                
-            }
-            if(result->next != nullptr){
-            result = result->next;
-            }
+            int sum = carry;
+            if(t1) sum = sum+ t1->val;
+            if(t2) sum = sum+ t2->val;
 
-            t1= (t1!= NULL)? t1->next:  t1;
-            t2= (t2!= NULL)? t2->next: t2;
+            ListNode* newNode = new ListNode(sum%10);
+            carry = sum/10;
+            curr->next = newNode;
+            curr = curr->next;
+
+            if(t1) t1= t1-> next;
+            if(t2) t2= t2->next;
         }
-        if (carryFlag==1)
+        if(carry)
         {
             ListNode* lastNode = new ListNode(carry);
-            result->next= lastNode;
+            curr->next = lastNode;
+            curr= curr->next;
         }
-        return newHead;
+        return dummyNode->next;
+        
     }
 };
