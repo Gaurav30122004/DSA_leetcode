4a2172562abfa2c4f51a73d5ea766991feaf67e8
Gaurav30122004 <gauravmahto3012@gmail.com>
2026-09-20 00:08:00 +0530

    Time: 3 ms (41.13%) | Memory: 13 MB (14.78%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -4,27 +4,34 @@ public:
         int n1 = nums1.size();
         int n2 = nums2.size();
 
-        vector<int> ans(n1);
-        for(int i=0; i<n1; i++)
+        vector<int> nge(n2);
+        unordered_map<int,int> mp;
+        stack<int> st;
+
+        for(int i=n2-1; i>=0; i--)
         {
-            bool flag= false;
-            for(int j=0; j<n2; j++)
+            while(!st.empty() && st.top() <= nums2[i])
             {
-                if(nums2[j] == nums1[i])
-                {
-                    flag = true;
-                }
-                if(flag)
-                {
-                    if(nums2[j] > nums1[i]) 
-                    {
-                        ans[i] = nums2[j];
-                        break;
-                    }
-                    ans[i] = -1;
-                }
+                st.pop();
             }
+            if(st.empty()) nge[i] = -1;
+            else{
+                nge[i] = st.top();
+            }
+            st.push(nums2[i]);
         }
-        return ans ;
+
+        for(int i=0; i<n2; i++)
+        {
+            mp[nums2[i]] = nge[i];
+        }
+
+        vector<int> ans;
+
+        for(int m: nums1)
+        {
+            ans.push_back(mp[m]);
+        }
+        return ans;   
     }
 };
